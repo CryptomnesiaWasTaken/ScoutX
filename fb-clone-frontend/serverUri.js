bda709a562290e2227915e99bf99d4156cffa9fe
Cryptomnesia <82593272+CryptomnesiaWasTaken@users.noreply.github.com>
2023-03-10 00:44:50 +0100

    Add files via upload

--- fb-clone-frontend/serverUri.js
+++ fb-clone-frontend/serverUri.js
@@ -1,3 +1,3 @@
 export default process.env.NODE_ENV === "development"
-	? "https://scoutxsoc.netlify.app"
-	: "https://scoutxsoc.netlify.app";
+	? "https://scoutx.onrender.com"
+	: "https://scoutx.onrender.com";
