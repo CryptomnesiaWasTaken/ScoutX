04a7a4fc12326a13cce6aa98938c21dc56556587
Cryptomnesia <82593272+CryptomnesiaWasTaken@users.noreply.github.com>
2023-03-11 01:56:54 +0100

    Update URI.js

--- fb-clone-backend/URI.js
+++ fb-clone-backend/URI.js
@@ -1,7 +1,7 @@
 const isDevelopmentMode = process.env.MODE === "DEV";
 
 const clientURL = isDevelopmentMode
-	? "http://localhost:3000"
-	: ["https://scoutx.onrender.com/", "https://scoutx.onrender.com/"];
+	? "https://scoutx.onrender.com"
+	: ["https://scoutx.onrender.com", "https://scoutx.onrender.com"];
 
 module.exports = { clientURL };
